7d80450764ec9459fb0433015cc6e985e5650564
RaynaldoHuang <huangraynaldo@gmail.com>
2025-03-19 16:49:20 +0700

    first commit

---
+++ components/main.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { images } from "@/lib/carousel";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+export default function Content() {
+    return (
+        <>
+            <section>
+                <div className="w-full mx-auto">
+                    <Swiper
+                        modules={[Navigation, Pagination, Autoplay]}
+                        spaceBetween={10}
+                        slidesPerView={1}
+                        navigation
+                        pagination={{ clickable: true }}
+                        autoplay={{ delay: 4000 }}
+                    >
+                        {images.map((src, index) => (
+                            <SwiperSlide key={index}>
+                                <img
+                                    src={src}
+                                    alt={`Slide ${index + 1}`}
+                                    className="w-full h-[585px] object-cover"
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            </section>
+        </>
+    );
+}
---
+++ lib/carousel.ts
@@ -0,0 +1,4 @@
+export const images = [
+    "/jpg/carousel1.jpg",
+    "/jpg/carousel1.jpg",
+];
